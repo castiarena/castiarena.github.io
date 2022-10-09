45c2dc4ecf2ffbb38ed7bbe49150d41c0342fd60
Agustin Castiarena <castiarena@gmail.com>
2022-10-09 17:47:58 +0200

    with lorem

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ export const App = () => {
     return (
         <div className="App">
             <h1>Portfolio hello world</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque autem corporis et excepturi illo labore magnam natus qui quidem quisquam repellendus sint tempora tempore ullam unde vel vitae voluptas, voluptatem.</p>
         </div>
     )
 }
