761b6d761f656ac0b3f0f6e5d37e1a6f1f8f8c04
Agustin Castiarena <castiarena@gmail.com>
2022-10-12 08:54:29 +0200

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {Avatar, Button, Heading, HStack, Link, Text, VStack} from "@chakra-ui/react";
+import {ChatIcon, DownloadIcon, EmailIcon, StarIcon} from "@chakra-ui/icons";
 import { Layout } from "./Layout";
 import face from './face.jpg'
 import cv from './curriculum-vitae.pdf'
@@ -16,25 +17,29 @@ export const App = () => {
                         mb={[6, 8,0]}
                     />
                 </VStack>
-                <VStack spacing={1} alignItems={"flex-start"}>
+                <VStack spacing={2} alignItems={["center","center","flex-start"]}>
                     <Heading mb={4}>Agustin Castiarena</Heading>
                     <Heading size="md">Frontend Team Lead</Heading>
 
                     <Button
+                        leftIcon={
+                        <ChatIcon
+                            color={"blue.400"}
+                        />
+                    }
                         variant={'link'}
+                        size={'sm'}
                         as={'a'}
-                        download={'Agustin Castiarena - resume.pdf'}
-                        href={cv}>
-                        Download resume
-                    </Button>
-
-                    <Button variant={'link'} as={'a'}  href="https://linkedin.com/in/agustin-castiarena" target="_blank">
+                        href="https://linkedin.com/in/agustin-castiarena"
+                        target="_blank">
                         linkedin.com/in/agustin-castiarena
                     </Button>
 
                     <Button
                         variant={'link'}
                         as={'a'}
+                        leftIcon={<EmailIcon color={'orangered'}/>}
+                        size={'sm'}
                         href="mailto:castiarena@gmail.com"
                         target="_blank">
                         castiarena@gmail.com
@@ -42,10 +47,20 @@ export const App = () => {
                     <Button
                         variant={'link'}
                         as={'a'}
-                        leftIcon={<>📍</>}
+                        size={'sm'}
+                        leftIcon={<StarIcon color={'green'}/>}
                         href="https://www.google.com.ar/maps/@41.3988958,2.1813664,13.24z"
                         target="_blank">
                         Barcelona - Spain
+                    </Button>
+                    <Button
+                        as={'a'}
+                        variant={'link'}
+                        size={'sm'}
+                        leftIcon={<DownloadIcon color={'teal'}/>}
+                        download={'Agustin Castiarena - resume.pdf'}
+                        href={cv}>
+                        Download resume
                     </Button>
                 </VStack>
             </HStack>
